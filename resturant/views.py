3a47af04b46d4b0676ad8f5de45d8a0dc2a1a214
Mir Khawar Ahmed <112721746+khawar2006@users.noreply.github.com>
2024-01-15 22:49:55 +0500

    Update views.py

--- resturant/views.py
+++ resturant/views.py
@@ -1,14 +1,18 @@
-from django.shortcuts import render
+site_details = {
+       'name':'Restaurant',
+       # 'year':date.today.year(),
+       'phone':'+92 333333333',
+       'email':'xyz@gmail.com'
+}
 
-# Create your views here.
 def index(request):
-       return render(request, 'index.html')
+       return render(request, 'index.html', {'site_details':site_details})
        
 def about(request):
-       return render(request, 'about.html')
+       return render(request, 'about.html', {'site_details':site_details})
 
 def menu(request):
-       return render(request, 'menu.html')
+       return render(request, 'menu.html', {'site_details':site_details})
 
 def book(request):
-       return render(request, 'book.html')
+       return render(request, 'book.html', {'site_details':site_details})
