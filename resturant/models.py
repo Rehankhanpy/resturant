931f65e6a447f32acdd0fa5da22e4ace35ed1f14
Mir Khawar Ahmed <khawrahmed2006@gmail.com>
2024-01-16 21:38:36 +0500

    Category in Menu

--- resturant/models.py
+++ resturant/models.py
@@ -5,6 +5,13 @@ from django.db import models
 class menu_items(models.Model):
        name = models.CharField(max_length=50, blank=False)
        price = models.IntegerField()
+       choices = {
+              'Burger':'Burger',
+              'Pizza':'Pizza',
+              'Pasta':'Pasta',
+              'Fries':'Fries'
+       }
+       category = models.CharField(max_length=10, blank=True, null=True, choices=choices)
        is_avalaible = models.BooleanField(default=True)
        description = models.TextField(max_length=300)
        image = models.ImageField(upload_to='images/menu_items_images', blank=True)
