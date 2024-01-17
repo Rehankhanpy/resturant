a486257fc0b57d88439c955b8d2e376d607bf93f
Mir Khawar Ahmed <khawrahmed2006@gmail.com>
2024-01-17 23:59:38 +0500

    Signup, SignIn and Logout Functionality Added

--- resturant/urls.py
+++ resturant/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
        path('about', views.about, name='about'),
        path('book', views.book, name='book'),
        path('menu', views.menu, name='menu'),
+       path('signup', views.signup, name='signup'),
+       path('signin', views.signin, name='signin'),
+       path('logout', views.logout_user, name='logout_user'),
+
 ]
