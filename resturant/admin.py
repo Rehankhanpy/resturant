834aff4d1391adc71232e8469f680d3bc4543925
Rehan <rehankhan181114@gmail.com>
2024-01-17 12:06:47 -0800

    added accounts model

--- resturant/admin.py
+++ resturant/admin.py
@@ -1,9 +1,21 @@
 from django.contrib import admin
-from .models import menu_items, Category
+from django.contrib.auth.admin import UserAdmin
+from .models import menu_items, Category, account
 
 # Register your models here.
+class accountadmin(UserAdmin):
+       list_display = ['username', 'email']
+       list_display_links = ['username', 'email']
+       readonly_fields = ['last_login', 'date_joined']
+       ordering = ('-username',)
 
-admin.site.register(menu_items)
+       filter_horizontal = ()
+       list_filter = ()
+       fieldsets = ()
 
+class menu_items_admin(admin.ModelAdmin):
+       list_display = ['name', 'price', 'is_avalaible']
 
+admin.site.register(menu_items, menu_items_admin)
+admin.site.register(account, accountadmin)
 admin.site.register(Category)
